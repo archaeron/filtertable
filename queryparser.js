d2057a14fc448bf5234b54bd00fb2f3548c1b34f
Archaeron <whatamaps@gmail.com>
2011-07-18 18:36:51 +0200

    begin queryparser

---
+++ queryparser.js
@@ -0,0 +1,16 @@
+// page: https://github.com/archaeron/filtertable
+// license: MIT (http://www.opensource.org/licenses/mit-license.php)
+(function(window)
+{
+
+	var queryparser = function(query)
+	{
+		console.log(query);
+		var opt = {};
+		
+		return opt;
+	};
+
+	window.queryparser = queryparser;
+
+}(window));
